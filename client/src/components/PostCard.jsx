d3e4c76097afc0837499984545fec5b405ee6ea7
chaeyun0426-dev <chaeyun0426@gmail.com>
2026-04-08 21:12:38 +0900

    feat: 게시글 css 추가

--- client/src/components/PostCard.jsx
+++ client/src/components/PostCard.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-
+import './PostCard.css'
 
 //포스트 카드 공통 컴포넌트
 // 포스트 카드> 헤더(제목, 북마크 버튼)>내용
@@ -12,7 +12,7 @@ const PostCard = ({ title, content }) => {
         setisBookmarked(!isBookmarked);
     }
     return (
-        <div className="-Udada-post-card">
+        <div className="Udada-post-card">
             <div className="Udada-post-header">
                 <h2 className="Udada-post-title">{title}</h2>
                 <button className={`Udada-bookmark-button${isBookmarked?'active':''}`} onClick={toggleBookmark}>
