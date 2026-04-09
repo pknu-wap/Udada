346df5322a9d8d0db383f46fd91506b656cc614d
kdk5925 <kdk01059259892@gmail.com>
2026-04-10 02:24:21 +0900

    작업 내용

---
+++ client/src/App.jsx
@@ -0,0 +1,20 @@
+import Navbar from "./components/Navbar";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div className="app">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ client/src/components/Sidebar.jsx
@@ -0,0 +1,13 @@
+import "./Sidebar.css";
+
+export default function Sidebar() {
+  return (
+    <div className="sidebar">
+      <h2 style={{ textAlign: "center" }}>UDADA</h2>
+
+      <div className="menu">홈</div>
+      <div className="menu">북마크</div>
+      <div className="menu">설정</div>
+    </div>
+  );
+}
