a617199b8436b0d2865eab2adfe5212c2e2dfb5f
kdk5925 <kdk01059259892@gmail.com>
2026-04-12 01:33:13 +0900

    feature: 인트로 페이지 UI 구현

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,18 +1,30 @@
-import Navbar from "./components/Navbar";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Intro from "./pages/Intro";
+import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 
 function App() {
   return (
     <BrowserRouter>
-      <div className="app">
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-        </Routes>
-      </div>
+      <Routes>
+        {/* 인트로 페이지 */}
+        <Route path="/" element={<Intro />} />
+
+        {/* 홈 페이지 (Navbar 포함) */}
+        <Route
+          path="/home"
+          element={
+            <>
+              <Navbar />
+              <Home />
+            </>
+          }
+        />
+
+        {/* 로그인 페이지 */}
+        <Route path="/login" element={<Login />} />
+      </Routes>
     </BrowserRouter>
   );
 }
