1cdd4646dee8f823a5fb03ca71230f55d80ef8a7
kdk5925 <kdk01059259892@gmail.com>
2026-04-11 12:07:28 +0900

    레이아웃 수정 (sidebar + content 정렬 문제 해결)

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,15 +1,3 @@
-<<<<<<< HEAD
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import "./index.css";
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-=======
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
@@ -20,4 +8,3 @@ createRoot(document.getElementById('root')).render(
     <App />
   </StrictMode>,
 )
->>>>>>> 58cfe2d0876b988a06a3632be306522f390668c4
