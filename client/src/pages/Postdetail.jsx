f4454ad4587e26e801e5d642eda31f9774e8a709
chaeyun0426-dev <chaeyun0426@gmail.com>
2026-04-15 01:43:53 +0900

    feat: 상세페이지.jsx 구현

---
+++ client/src/pages/Postdetail.jsx
@@ -0,0 +1,53 @@
+import "./Postdetail.css";
+import { useParams, useNavigate } from "react-rounter-dom";
+import Navbar from client / src / components / Sidebar.jsx;
+
+const Postdetail = () => {
+    const { id } = useParams();
+    const navigate = useNavigate();
+
+    const [Post, setPost] = useState({
+        id: id,
+        title: "제목입니다",
+        content: "공지사항입니다 감기조심하세요..",
+        categoryId: 1,
+        categoryName: "개인",
+        originalUrl: "공지링크가들어가다",
+        noticedAt: "2026-04-15",
+        isBookmarked: false
+    });
+    const toggleBookmark = () => {
+        setPost({ ...post, isBookmarked: !post.isBookmarked });
+    };
+
+    return (
+        <div className="post-detail-button">
+            <button className="back-button" onClick={() => navigate(-1)}>
+                {/* 뒤로 돌아가기 */}
+            </button>
+
+            <article claaName="detail-article">
+                <header className="article-header">
+                    <div className="header-top">
+                        <hi className="article-title">{post.title}</hi>
+                        <button
+                            className={`bookmark-btn ${post.isBookmarked ? "active" : ""}`}
+                            onClick={toggleBookmark}
+                        >
+                            {post.isBookmarked ? "북마크됨" : "북마크안됨"}
+                        </button>
+                    </div>
+                    <div className="header-bottom">
+                        <span className="category-tag">{post.categroyName}</span>
+                        <span className="write-date">{post.noticedAt}</span>
+                    </div>
+                </header>
+                <hr className="content-divider"/>
+                <section className="article-body">
+                    {post.content}
+                </section>
+            </article>
+
+        </div>
+    )
+}
