f6013f5baa38ad86ae52b33d1346b703bc23115c
chaeyun0426-dev <chaeyun0426@gmail.com>
2026-04-15 02:22:41 +0900

    fix: 포스트카드 컴포넌트 수정

--- client/src/components/PostCard.jsx
+++ client/src/components/PostCard.jsx
@@ -1,22 +1,27 @@
 import React, { useState } from 'react';
 import './PostCard.css'
 
-//포스트 카드 공통 컴포넌트
-// 포스트 카드> 헤더(제목, 북마크 버튼)>내용
+//포스트 카드 공통 컴포넌트 목록에서 사용
+// 포스트 카드> 헤더(제목, 북마크 버튼)
 //북마크 T/F (토글)
 
-const PostCard = ({ title, content }) => {
+const PostCard = ({ title, date, category }) => {
     const [isBookmarked, setisBookmarked] = React.useState(false);
 
-    const toggleBookmark = () => {
+    const toggleBookmark = (e) => {
+        e.stopPropagation();
         setisBookmarked(!isBookmarked);
     }
     return (
         <div className="Udada-post-card">
             <div className="Udada-post-header">
+                <div className="Udada-post-info">
+                    <span className="Udada-post-category">{category}</span>
+                    <span className="Udada-post-date">{date}</span>
+                </div>
                 <h2 className="Udada-post-title">{title}</h2>
-                <button className={`Udada-bookmark-button${isBookmarked?'active':''}`} onClick={toggleBookmark}>
-                    {isBookmarked ? '1' : '2'}
+                <button className={`Udada-bookmark-button${isBookmarked ? 'active' : ''}`} onClick={toggleBookmark}>
+                    {isBookmarked ? '북마트됨' : '북마크안됨'}
                 </button>
             </div>
 
