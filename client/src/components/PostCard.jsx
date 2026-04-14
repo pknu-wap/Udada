cfbbb5043e41e3c4ee863e5821623c552abb291e
chaeyun0426-dev <chaeyun0426@gmail.com>
2026-04-15 02:41:52 +0900

    fix: 포스트카드 jsx,css 전체수정

--- client/src/components/PostCard.jsx
+++ client/src/components/PostCard.jsx
@@ -5,7 +5,7 @@ import './PostCard.css'
 // 포스트 카드> 헤더(제목, 북마크 버튼)
 //북마크 T/F (토글)
 
-const PostCard = ({ title, date, category }) => {
+const PostCard = ({ id, title, date, category }) => {
     const [isBookmarked, setisBookmarked] = React.useState(false);
 
     const toggleBookmark = (e) => {
@@ -14,21 +14,18 @@ const PostCard = ({ title, date, category }) => {
     }
     return (
         <div className="Udada-post-card">
-            <div className="Udada-post-header">
-                <div className="Udada-post-info">
-                    <span className="Udada-post-category">{category}</span>
-                    <span className="Udada-post-date">{date}</span>
-                </div>
-                <h2 className="Udada-post-title">{title}</h2>
-                <button className={`Udada-bookmark-button${isBookmarked ? 'active' : ''}`} onClick={toggleBookmark}>
-                    {isBookmarked ? '북마트됨' : '북마크안됨'}
-                </button>
+            <div className="Udada-post-id">{id}</div>
+            <div className="post-title-wrapper">
+                <span className="Udada-post-title">
+                    [{category}]{title}
+                </span>
             </div>
-
-            <div className="Udada-post-content">
-                <p>{content}</p>
-            </div>
+            <div className="Udada-post-date">{date}</div>
+            <button className={`Udada-bookmark-button${isBookmarked ? 'active' : ''}`} onClick={toggleBookmark}>
+                {isBookmarked ? '북마트됨' : '북마크안됨'}
+            </button>
         </div>
+
     );
 };
 
