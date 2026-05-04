801c356336e54483ded7a776c3888f3789ba63ef
chaeyun0426-dev <chaeyun0426@gmail.com>
2026-05-04 21:31:47 +0900

    fix: Navbar.jsx 수정

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,10 +1,28 @@
+import React from "react";
 import { Link } from "react-router-dom";
 
-export default function Navbar() {
+
+export default function Navbar({ toggleKeywordPanel }) {
   return (
-    <div>
-      <Link to="/">홈</Link> |{" "}
-      <Link to="/login">로그인</Link>
+    <div className="navbar-container">
+      {/* 로고 왼쪽 배치 */}
+      <div className="navbar-left"></div>
+
+      <div className="navbar-right">
+        <div className="search-section">
+          <div className="search-bar">
+            <input type="text" className="search-input" placeholder="검색어를 입력" />
+            <button className="search-btn">🔍</button>
+          </div>
+
+          <button className="keyword-btn" onClick={toggleKeywordPanel}>
+            # 키워드 설정
+          </button>
+
+          {/* 구분선 및 로그인 */}
+          <div className="nav-divider"></div>
+        </div>
+      </div>
     </div>
   );
 }
