93306a2ed9290bf744101bf7d428c1bdc029f02b
chaeyun0426-dev <chaeyun0426@gmail.com>
2026-05-06 02:17:50 +0900

    feat: 필터링버튼 활성화 추가

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -7,7 +7,7 @@ export default function Navbar({ toggleKeywordPanel }) {
   // 카테고리 활성화 상태 관리
   const [isActive, setIsActive] = useState(false);
 
-  const handleCategoryClick = () => {
+  const handleFilterClick = () => {
     setIsActive(!isActive); // 색상 토글
     if (toggleKeywordPanel) toggleKeywordPanel(); // 패널 열기 기능 실행
   };
@@ -23,16 +23,17 @@ export default function Navbar({ toggleKeywordPanel }) {
 
       <div className="navbar-center">
         <div className="search-section">
-          {/* 카테고리 버튼 (회색 -> 클릭 시 붉은색) */}
-          <button
-            className={`category-btn ${isActive ? "active" : ""}`}
-            onClick={handleCategoryClick}
-          >
-            필터링
-          </button>
-          {/* 검색바 */}
-
-          <div className="search-wrapper">
+          <div className={`search-wrapper ${isActive ? "active" : ""}`}>
+            
+            {/* 필터링 버튼 (회색 -> 클릭 시 붉은색) */}
+            <button
+              className={`Filter-btn ${isActive ? "active" : ""}`}
+              onClick={handleFilterClick}
+            >
+              필터링
+            </button>
+            {/* 검색바 */}
+            <div className="divider" />
             <input type="text" className="search-input" placeholder="검색어를 입력" />
             <button className="search-icon-btn">🔍</button>
           </div>
