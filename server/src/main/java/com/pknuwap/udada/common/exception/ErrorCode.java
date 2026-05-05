0f4c21e4af78cff140036b05b7672d356864c02d
sseung416 <choiseungyeon416@gmail.com>
2026-05-05 15:14:55 +0900

    fix: 카카오 SNS 로그인 API 400 오류도 모두 500 오류로 뜨는 이슈 해결

--- server/src/main/java/com/pknuwap/udada/common/exception/ErrorCode.java
+++ server/src/main/java/com/pknuwap/udada/common/exception/ErrorCode.java
@@ -6,8 +6,9 @@ import org.springframework.http.HttpStatus;
 @Getter
 public enum ErrorCode {
 
-    // 공통
+    // 토큰
     INVALID_TOKEN(HttpStatus.UNAUTHORIZED, "로그인된 회원 정보가 없습니다. access token을 잘 작성했는지 확인해주세요."),
+    INVALID_KAKAO_TOKEN(HttpStatus.BAD_REQUEST, "잘못된 code입니다."),
 
     // 북마크
     BOOKMARK_ALREADY(HttpStatus.BAD_REQUEST, "이미 북마크한 공지사항입니다."),
