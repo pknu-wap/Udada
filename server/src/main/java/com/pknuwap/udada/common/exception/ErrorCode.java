2eb4c39e668f60efb790ef3b36992dbbdc44f76b
sseung416 <choiseungyeon416@gmail.com>
2026-05-29 18:38:24 +0900

    refactor: 북마크 중복 예외 상태코드 400에서 409로 변경

--- server/src/main/java/com/pknuwap/udada/common/exception/ErrorCode.java
+++ server/src/main/java/com/pknuwap/udada/common/exception/ErrorCode.java
@@ -11,14 +11,14 @@ public enum ErrorCode {
     INVALID_KAKAO_TOKEN(HttpStatus.BAD_REQUEST, "잘못된 code입니다."),
 
     // 북마크
-    BOOKMARK_ALREADY(HttpStatus.BAD_REQUEST, "이미 북마크한 공지사항입니다."),
+    BOOKMARK_ALREADY(HttpStatus.CONFLICT, "이미 북마크한 공지사항입니다."),
     BOOKMARK_INVALID(HttpStatus.BAD_REQUEST, "존재하지 않는 북마크입니다."),
     BOOKMARK_NOT_OWNED(HttpStatus.BAD_REQUEST, "본인의 북마크만 삭제할 수 있습니다."),
 
     // 공지사항
     NOTICE_INVALID(HttpStatus.BAD_REQUEST, "존재하지 않는 공지사항입니다."),
 
-    //키워드
+    // 키워드
     KEYWORD_NOT_FOUND(HttpStatus.BAD_REQUEST, "존재하지 않는 키워드입니다."),
     KEYWORD_NOT_OWNED(HttpStatus.BAD_REQUEST, "본인의 키워드만 수정할 수 있습니다."),
 
