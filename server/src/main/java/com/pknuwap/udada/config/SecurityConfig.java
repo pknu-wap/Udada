f2f7b3ed3e4c6299235de6cc32de9280082fa103
sseung416 <choiseungyeon416@gmail.com>
2026-05-31 15:24:02 +0900

    fix: EmailVerificationFilter security config에 추가되지 않은 이슈 해결

--- server/src/main/java/com/pknuwap/udada/config/SecurityConfig.java
+++ server/src/main/java/com/pknuwap/udada/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.pknuwap.udada.config;
 
+import com.pknuwap.udada.common.email.EmailVerificationFilter;
 import com.pknuwap.udada.common.jwt.JwtAuthenticationFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
@@ -12,10 +13,11 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 
 @Configuration
 @EnableWebSecurity
-@RequiredArgsConstructor // 추가
+@RequiredArgsConstructor
 public class SecurityConfig {
 
-    private final JwtAuthenticationFilter jwtAuthenticationFilter; // 필터 주입
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+    private final EmailVerificationFilter emailVerificationFilter;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -24,12 +26,16 @@ public class SecurityConfig {
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(
                                 "/api/v1/auth/**",
+                                "/api/v1/users/email",
                                 "/swagger-ui/**",
                                 "/v3/api-docs/**"
-                        ).permitAll().anyRequest().authenticated()
+                        ).permitAll()
+                        .anyRequest().authenticated()
                 )
                 .formLogin(AbstractHttpConfigurer::disable)
-                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+                // JWT 필터 → 이메일 검증 필터 순서
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
+                .addFilterAfter(emailVerificationFilter, JwtAuthenticationFilter.class);
 
         return http.build();
     }
